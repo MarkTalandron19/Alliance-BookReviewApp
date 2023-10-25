a0f9a35ddfdb11b2964233d67cf81b9cfb364cea
Aloysius Beronque <aloysiusmatthew1@gmail.com>
2023-10-26 00:42:32 +0800

    Fixed modal popup

---
+++ ASI.Basecode.WebApp/wwwroot/js/Book.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('#AddBookBtn').click(function () {
+        /*jQuery.noConflict();*/
+
+        $('#BookModal').modal('show');
+        $('#modalTitle').text('Add a Book');
+        console.log("Button clicked");
+    });
+});
