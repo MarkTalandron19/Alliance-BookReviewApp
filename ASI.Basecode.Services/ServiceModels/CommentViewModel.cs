d797888ea3541f85cad8801360b37687e39341c4
Mark Allen Talandron <markallen.talandron.20@usjr.edu.ph>
2023-10-28 23:29:04 +0800

    created comment service

---
+++ ASI.Basecode.Services/ServiceModels/CommentViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ASI.Basecode.Services.ServiceModels
+{
+    public class CommentViewModel
+    {
+        [JsonPropertyName("commentId")]
+        [Required(ErrorMessage = "Comment Id is required.")]
+        public string commentId { get; set; }
+
+        [JsonPropertyName("commenterFirstName")]
+        [Required(ErrorMessage = "Commenter first name is required.")]
+        public string commenterFirstName { get; set; }
+
+        [JsonPropertyName("commenterLastName")]
+        [Required(ErrorMessage = "Commenter last name is required.")]
+        public string commenterLastName { get; set; }
+
+        [JsonPropertyName("comment")]
+        [Required(ErrorMessage = "Comment is required.")]
+        public string comment { get; set; }
+
+        [JsonPropertyName("reviewId")]
+        [Required(ErrorMessage = "Review Id is required.")]
+        public string reviewId { get; set; }
+    }
+}
