4f5be1c4d0caa94990d68c08db82c05b977a5974
MarkTalandron19 <113022659+MarkTalandron19@users.noreply.github.com>
2023-11-08 22:07:50 +0800

    identity study 1

--- ASI.Basecode.WebApp/Startup.cs
+++ ASI.Basecode.WebApp/Startup.cs
@@ -24,6 +24,60 @@ using System.Text;
 
 namespace ASI.Basecode.WebApp
 {
+    public partial class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            this.ConfigureDependencies(services);       // Configuration for dependency injections           
+            this.ConfigureDatabase(services);           // Configuration for database connections
+            this.ConfigureMapper(services);             // Configuration for entity model and view model mapping
+            this.ConfigureCors(services);               // Configuration for CORS
+            this.ConfigureAuth(services);               // Configuration for Authentication logic
+            this.ConfigureMVC(services);                // Configuration for MVC                  
+
+
+            // Add services to the container.
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
+            services.AddMvc(options => options.EnableEndpointRouting = false);
+
+            //services.AddLogging(x => x.AddConfiguration(Configuration.GetLoggingSection()).AddConsole().AddDebug());
+            PathManager.Setup(this.Configuration.GetSetupRootDirectoryPath());
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");     // Enables site to redirect to page when an exception occurs
+                app.UseHsts();                              // Enables the Strict-Transport-Security header.
+            }
+
+            this.ConfigureLogger(app, env);
+            app.UseStaticFiles();           // Enables the use of static files
+            app.UseHttpsRedirection();      // Enables redirection of HTTP to HTTPS requests.
+            app.UseCors("CorsPolicy");      // Enables CORS                              
+            app.UseRouting();
+            app.UseAuthentication();        // Enables the ConfigureAuth service.
+            app.UseMvc();
+            app.UseAuthorization();
+
+            this.ConfigureRoutes(app);      // Configuration for API controller routing
+            this.ConfigureAuth(app);        // Configuration for Token Authentication
+        }
+    }
     /// <summary>
     /// For configuring services on application startup.
     /// </summary>
@@ -107,7 +161,7 @@ namespace ASI.Basecode.WebApp
             });
 
             // DI Services AutoMapper(Add Profile)
-            this.ConfigureAutoMapper();
+            //this.ConfigureAutoMapper();
 
             // DI Services
             this.ConfigureOtherServices();
@@ -134,7 +188,6 @@ namespace ASI.Basecode.WebApp
 
         }
 
-
         /// <summary>
         /// Configure application
         /// </summary>
@@ -150,7 +203,7 @@ namespace ASI.Basecode.WebApp
                 this._app.UseHsts();
             }
 
-            this.ConfigureLogger();
+            //this.ConfigureLogger();
 
             this._app.UseTokenProvider(_tokenProviderOptions);
 
