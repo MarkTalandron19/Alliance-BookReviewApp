8627e5b3edea45bf3ae516c2110c87e8dc64835f
Matthaeuz <85921531+Matthaeuz@users.noreply.github.com>
2023-12-03 03:10:59 +0800

    Forgot Password Fixes

---
+++ ASI.Basecode.WebApp/Models/ForgotPasswordCodeViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace ASI.Basecode.WebApp.Models
+{
+    public class ForgotPasswordCodeViewModel
+    {
+        [JsonPropertyName("code")]
+        [Required(ErrorMessage = "Code is required.")]
+        public string Code { get; set; }
+
+        [JsonPropertyName("token")]
+        [Required(ErrorMessage = "Token is required.")]
+        public string Token { get; set; }
+
+        public string Email { get; set; }
+        public string OriginalToken { get; set; }
+    }
+}
